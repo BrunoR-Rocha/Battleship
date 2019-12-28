0830c5ffa2132509b63cfd8b49b59207650e0dda
Claudio Duarte Rodrigues <cluadio.duarte.98@live.com.pt>
2019-12-28 15:37:01 +0000

    First commit
    My working copy with Initial page  of grid and chat on Vue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var User = require('./model/User');
 
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
-const bcrypt = require("bcryptjs"); 
+const bcrypt = require("bcryptjs");
 
 const PORT = 3000;
 
@@ -23,7 +23,7 @@ const connections = [];
 var collections;
 
 
-io.sockets.on('connection',(socket) => {
+io.sockets.on('connection', (socket) => {
    connections.push(socket);
    console.log(' %s sockets is connected', connections.length);
 
@@ -34,33 +34,37 @@ io.sockets.on('connection',(socket) => {
    socket.on('sending message', (message) => {
       console.log('Message is received :', message);
 
-      io.sockets.emit('new message', {message: message});
+      io.sockets.emit('new message', {
+         message: message
+      });
    });
 });
 
 
-mongoUtils.conectToServer(function(){
-      console.log("Listening on port 3000");
+mongoUtils.conectToServer(function () {
+   console.log("Listening on port 3000");
 });
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-   res.sendFile(__dirname +"/views"+ '/welcome.html');
+   res.sendFile(__dirname + "/views" + '/welcome.html');
 });
 
-app.get('/register', (req, res) =>{
-   res.sendFile(__dirname +"/views"+ '/register.html');
+app.get('/register', (req, res) => {
+   res.sendFile(__dirname + "/views" + '/register.html');
 });
 
-app.get('/login', (req, res) =>{
-   res.sendFile(__dirname +"/views"+ '/login.html');
+app.get('/login', (req, res) => {
+   res.sendFile(__dirname + "/views" + '/login.html');
 });
 
-app.get('/main', (req, res) =>{
-   res.sendFile(__dirname +"/views"+ '/main.html');
+app.get('/main', (req, res) => {
+   res.sendFile(__dirname + "/views" + '/main.html');
 });
 
 app.get('/game',(req,res)=>{
@@ -75,58 +79,58 @@ app.post('/register', function (req,res){
    var email = req.body.email;
    var password = req.body.password;
    var password2 = req.body.password2;
-   
-   if(!name || !email || !password){
+
+   if (!name || !email || !password) {
       console.log("Incomplete Information");
-   }  
-   else if(password == password2){
-   
-         bcrypt.hash(password, 8, (err, hashedPassword) => { 
-            if (err) { 
-               return err; 
-            } 
-            var dados = {
-               "name": name,
-               "email":email,
-               "password": hashedPassword,
-            }
+   } else if (password == password2) {
 
-            collections = mongoUtils.getDriver();
+      bcrypt.hash(password, 8, (err, hashedPassword) => {
+         if (err) {
+            return err;
+         }
+         var dados = {
+            "name": name,
+            "email": email,
+            "password": hashedPassword,
+         }
 
-            users.push(dados);
-            console.log(users);
+         collections = mongoUtils.getDriver();
 
-            collections.collection('users').insertOne(dados);
+         users.push(dados);
+         console.log(users);
 
-            res.redirect('/main');
-         });
-   }
-   else{
+         collections.collection('users').insertOne(dados);
+
+         res.redirect('/main');
+      });
+   } else {
       console.log("Erro de insercao");
-   }   
+   }
 });
 
-app.post('/login', function (req,res) {
+app.post('/login', function (req, res) {
 
    var name = req.body.email;
    var password = req.body.password;
 
    collections = mongoUtils.getDriver();
 
-   collections.collection('users').find({email:name}).toArray(function(err, result){
+   collections.collection('users').find({
+      email: name
+   }).toArray(function (err, result) {
 
-      if(result[0] == undefined){
+      if (result[0] == undefined) {
          res.redirect('/login');
-      }else{
-           // console.log(result[0].password);
-            bcrypt.compare(password, result[0].password, (err, isMatch) => { 
-               //se as palavra passe coincidir com a da base de dados
-               if(isMatch){
-                  res.redirect('/main'); // passa os valores do utilizador - Nome e email
-               }else{
-                  res.redirect('/login'); // mostrar mensagem de erro - password incorreta
-               }
-            });
-         }
-      })
+      } else {
+         // console.log(result[0].password);
+         bcrypt.compare(password, result[0].password, (err, isMatch) => {
+            //se as palavra passe coincidir com a da base de dados
+            if (isMatch) {
+               res.redirect('/main'); // passa os valores do utilizador - Nome e email
+            } else {
+               res.redirect('/login'); // mostrar mensagem de erro - password incorreta
+            }
+         });
+      }
+   })
 });
