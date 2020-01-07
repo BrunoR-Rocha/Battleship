6051bc2355494a62c696f30250faa7e0d70de6d2
HannibalLecter <brunor_gon_rocha@hotmail.com>
2020-01-07 13:59:13 +0000

    Revert "some changes"
    This reverts commit 79dee28cf9cffb6dd873f65f7b67403aa5fe6b20.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var http     = require('http');
 var ejs      = require('ejs');
 var socketio = require('socket.io');
 var bodyParser = require('body-parser');
-var Battle = require('./js/battle.js');
 
 var app = express();
 app.set('view engine','ejs');
@@ -22,7 +21,6 @@ server.listen(PORT, function(){
 });
 
 var users = [];
-var numJogo = 1;
 var actual_user = [];
 var collections;
 
@@ -64,63 +62,66 @@ app.get('/mygames',(req,res)=>{
 
 io.on('connection',(socket) => {
    console.log('Someone joined the server'); 
-   //console.log(users);
-
-   users[socket.id] = {
-      game: null,
-      jogador: null
-    }; 
-
+   console.log(users);
 
    socket.join('waiting players');
 
-   
    //joinWaitingPlayers();
 
    socket.on('join',function(nome){
       users[socket.id] = nome;
       console.log(users[socket.id]+' joined the chatroom'); 
-      //console.log(users);
+      console.log(users);
 
-      //joinPlayers();
-      var players = getGamersWaiting('waiting players');
-      //console.log(users);
-      console.log(players.length);
-      /*if(players.length >= 2){
-        // io.emit('start');
-         var jogo = new Battle(numJogo++, players[0], players[1]);
-         //console.log(jogo);
+     /* var players = getGamersWaiting('waiting players');
 
-         //sair da sala de espera para iniciar o jogo
-         players[0].leave('waiting players');
-         players[1].leave('waiting players');
-
-         players[0].join('game' + jogo.id);
-         players[1].join('game'+ jogo.id);
-
-         /*
-            users[players[0].id].game = jogo;
-            users[players[1].id];
-            console.log(users[players[0].id].game);
-         */
-
-        /* console.log(io.sockets.adapter.rooms['game'+ jogo.id]);
-         io.to('game'+jogo.id).emit('start', jogo.id);
-
-         
+      if(players.length >= 2){
+         io.emit('start');
       }*/
-
       //io.emit('update'," ### "+users[socket.id]+" is prepared for battle  ###");
   });
-
-  socket.on('sending message', (message) => {
-   console.log('Message is received :', message);
-   
-   io.emit('chat message server', {message: message});
-});
-   
 });
 
+function joinWaitingPlayers() {
+   var players = getGamersWaiting('waiting players');
+   
+   console.log("waiting");
+   console.log(io.sockets.adapter.rooms['waiting players']);
+
+  if(players.length >= 2) {
+   //console.log(players);
+
+   //criar um novo jogo entre os 2 jogadores
+
+   //sair da sala de espera para iniciar o jogo
+   players[0].leave('waiting players');
+   players[1].leave('waiting players');
+   
+   console.log("game");
+   //criaçao de uma sala, com um id especifico para que ambos os jogadores joguem entre si sem interferencias
+
+   players[0].join('game');
+   players[1].join('game');
+
+   console.log(io.sockets.adapter.rooms['game']);
+
+   /* var game = new BattleshipGame(gameIdCounter++, players[0].id, players[1].id);
+ 
+     users[players[0].id].player = 0;
+     users[players[1].id].player = 1;
+     users[players[0].id].inGame = game;
+     users[players[1].id].inGame = game;
+     
+     io.to('game' + game.id).emit('join', game.id);
+ 
+     // send initial ship placements
+     io.to(players[0].id).emit('update', game.getGameState(0, 0));
+     io.to(players[1].id).emit('update', game.getGameState(1, 1));
+ 
+     console.log((new Date().toISOString()) + " " + players[0].id + " and " + players[1].id + " have joined game ID " + game.id);
+   */
+   }
+ }
 
  function getGamersWaiting(room) {
 
@@ -163,7 +164,7 @@ app.post('/register', function (req,res){
 
             collections.collection('users').insertOne(dados);
 
-            res.redirect('/login');
+            res.redirect('/main');
          });
    }
    else{
@@ -200,4 +201,3 @@ app.post('/main', function (req,res) {
          }
       })
 });
-
